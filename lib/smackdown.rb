3a6aad571f4cd4b9f47a3d76ee8a513e4bd299dc
Rob Esris <resris@paperlesspost.com>
2016-06-02 16:50:00 -0400

    Added version

--- lib/smackdown.rb
+++ lib/smackdown.rb
@@ -3,4 +3,5 @@ require 'smackdown/file_coverage_diff'
 require 'smackdown/coverage_diff_reporter'
 
 module Smackdown
+  VERSION = File.read(File.join(File.dirname(__FILE__), 'smackdown', 'version.txt')).strip
 end
