5851ce1c62e928bf348ad00d522c5d2c3e839ad0
Rob Esris <resris@paperlesspost.com>
2016-07-26 12:39:01 -0400

    More tests and some refactoring

--- test/test_smackdown.rb
+++ test/test_smackdown.rb
@@ -1,19 +1,7 @@
-require 'minitest/autorun'
-require 'smackdown'
+require "minitest/autorun"
+require "smackdown"
 
-describe Smackdown do
-  before do
-    repo_path = File.join(File.dirname(__FILE__), 'smackdown_test_repo')
-
-    @reporter = Smackdown::CoverageDiffReporter.new(
-      repo_path,
-      head: 'my_branch',
-      merge_base: 'master'
-    )
-    @reporter.run
-    @uncovered_file = @reporter.file_coverage_diffs["lib/class_with_uncovered_code_added.rb"]
-  end
-
+def check_results
   describe "uncovered changes" do
     describe "introduced in my_branch" do
       it "reports that the file is not covered" do
@@ -36,6 +24,105 @@ describe Smackdown do
         end
         refute result
       end
+    end
+  end
+
+  describe "displaying a report" do
+    before do
+      @report_output = ""
+      @reporter.report do |file_coverage_diff|
+        @report_output += "#{file_coverage_diff.relative_path}: #{file_coverage_diff.uncovered_lines.count} uncovered line(s).\n"
+      end
+    end
+
+    it "should report accurate information about the coverage" do
+      assert_match(/lib\/class_with_uncovered_code_added\.rb\: 1 uncovered line\(s\)\./, @report_output)
+    end
+  end
+end
+
+describe "normal usage" do
+  before do
+    @repo_path = File.join(File.dirname(__FILE__), "smackdown_test_repo")
+  end
+
+  describe "using a test coverage report file path" do
+    describe "on the local filesystem" do
+      before do
+        @reporter = Smackdown::CoverageDiffReporter.new(
+          @repo_path,
+          head: "my_branch",
+          merge_base: "master"
+        )
+        @reporter.run
+        @uncovered_file = @reporter.file_coverage_diffs["lib/class_with_uncovered_code_added.rb"]
+      end
+
+      check_results
+    end
+
+    describe "at a remote url" do
+      before do
+        url = "http://mycoveragereports.com/reports/1"
+
+        # Fake the http call
+        coverage_report_path = File.join(@repo_path, "coverage", "coverage.json")
+        coverage_json = File.read(coverage_report_path)
+        Net::HTTP.expects(:get).with(url).returns(coverage_json)
+
+        @reporter = Smackdown::CoverageDiffReporter.new(
+          @repo_path,
+          coverage_report_path: url,
+          head: "my_branch",
+          merge_base: "master"
+        )
+        @reporter.run
+        @uncovered_file = @reporter.file_coverage_diffs["lib/class_with_uncovered_code_added.rb"]
+      end
+
+      check_results
+    end
+  end
+
+  describe "using a json string" do
+    before do
+      coverage_report_path = File.join(@repo_path, "coverage", "coverage.json")
+      coverage_json = File.read(coverage_report_path)
+
+      @reporter = Smackdown::CoverageDiffReporter.new(
+        @repo_path,
+        head: "my_branch",
+        merge_base: "master",
+        coverage_json: coverage_json
+      )
+      @reporter.run
+      @uncovered_file = @reporter.file_coverage_diffs["lib/class_with_uncovered_code_added.rb"]
+    end
+
+    check_results
+  end
+end
+
+describe "error conditions" do
+  describe "attempting to initialize with both a coverage report path and a json string" do
+    before do
+      @repo_path = File.join(File.dirname(__FILE__), "smackdown_test_repo")
+      coverage_report_path = File.join(@repo_path, "coverage", "coverage.json")
+      @coverage_json = JSON.parse(File.read(coverage_report_path))
+    end
+
+    it "raises an error" do
+      error = assert_raises(RuntimeError) do
+        reporter = Smackdown::CoverageDiffReporter.new(
+          @repo_path,
+          head: "my_branch",
+          merge_base: "master",
+          coverage_report_path: @repo_path,
+          coverage_json: @coverage_json
+        )
+      end
+
+      assert_equal "Please pass only :coverage_report_path or :coverage_json, not both.", error.message
     end
   end
 end
