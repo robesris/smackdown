6c05a2dbc00329da99035644c1e2bd992496b353
Rob Esris <resris@paperlesspost.com>
2016-07-29 14:41:53 -0400

    Auto-include smackdown task

--- lib/smackdown.rb
+++ lib/smackdown.rb
@@ -6,4 +6,13 @@ require 'smackdown/file_coverage_diff'
 require 'smackdown/coverage_diff_reporter'
 
 module Smackdown
+  class Tasks
+    include Rake::DSL if defined? Rake::DSL
+
+    def install_tasks
+      import File.join(File.dirname(__FILE__), 'tasks/smackdown.rake')
+    end
+  end
 end
+
+Smackdown::Tasks.new.install_tasks
